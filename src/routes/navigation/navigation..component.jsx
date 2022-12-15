2ea40cb6c3e1f20ec2837afa2e4f6cfcebc5a5d6
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-15 21:13:36 +0200

    cart dropdown and cart context added for toggle cart on and off

--- src/routes/navigation/navigation..component.jsx
+++ src/routes/navigation/navigation..component.jsx
@@ -1,15 +1,21 @@
 import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.util";
 
 import {ReactComponent as CrownLogo } from '../../assets/crown.svg'; 
 import './navigation.styles.scss'
 
+
 const Navigation = () => {
   const {currentUser} = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
   
   return (
       <Fragment>
@@ -23,8 +29,11 @@ const Navigation = () => {
                   currentUser ? (
                     <span className="nav-link" onClick={signOutUser}>Sign out</span>
                   ) : (<Link className="nav-link" to='/auth'>SIGN IN</Link>
-                  )}   
+                  )}
+                  <CartIcon />   
             </div>
+            {isCartOpen &&  <CartDropdown />}
+            
         </div>
         <Outlet />
       </Fragment>
