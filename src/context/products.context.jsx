313c36066260882d719124a80f383e857f9c14d9
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-18 14:12:18 +0200

    collection and write batch implement to load data to firebase

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,6 +1,9 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.util.js';
+
+//import PRODUCTS from '../shop-data.json';
+import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
     products: [],
@@ -8,7 +11,12 @@ export const ProductsContext = createContext({
 })
 
 export const ProductsProvider = ({children}) => {
-    const [products] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+    
+    useEffect(()=> {
+        addCollectionAndDocuments('categories', SHOP_DATA)
+    }, []);
+        
     const value = {products};
 
     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
