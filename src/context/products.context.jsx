1e3ecc2d91023ffc27138f0b787567492523011c
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-18 11:23:23 +0200

    product context Products is converted to products

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 import PRODUCTS from '../shop-data.json';
 
 export const ProductsContext = createContext({
-    Products: [],
+    products: [],
     
 })
 
