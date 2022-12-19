6e0d001aeb98bd6c89b065c834cd1cddbd26bbe5
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-19 06:31:16 +0200

    product context converted to categories context

---
+++ src/context/categories.context.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from 'react';
+
+/* import { addCollectionAndDocuments } from '../utils/firebase/firebase.util.js'; */
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.util.js';
+
+//import PRODUCTS from '../shop-data.json';
+/* import SHOP_DATA from '../shop-data.js'; */
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+    
+})
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+    
+   /*  useEffect(()=> {
+        addCollectionAndDocuments('categories', SHOP_DATA)
+    }, []); */
+
+    useEffect(()=> {
+        const getCategoriesMap = async() =>{ 
+            const categoriesMap = await getCategoriesAndDocuments();
+            
+            setCategoriesMap(categoriesMap)
+        }
+        getCategoriesMap();
+    }, [])
+        
+    const value = {categoriesMap};
+
+    return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+}
