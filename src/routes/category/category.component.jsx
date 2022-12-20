4fbed22f2b7bad4b2f40c18726b4014899884408
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-20 09:25:53 +0200

    nested route and link included

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 import { CategoriesContext } from "../../context/categories.context";
 import { useParams } from "react-router-dom";
 import "./category.styles.scss";
@@ -14,12 +14,15 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
   );
 };
 
